8d4aa51586c9b3c688b5ab78bfb1aa666c818a33
Uzair Ali <ghayasali2424@gmail.com>
2026-01-11 21:44:27 +0500

    styling update

--- server/data/users.js
+++ server/data/users.js
@@ -1,8 +1,5 @@
-// In-memory user storage
-// In production, this would be replaced with a database
+
 export const users = [];
 
-// Note: In production, you would initialize users from a database
-// For demo purposes, you can create an admin user through the signup form
-// with role: 'admin'
+
 
